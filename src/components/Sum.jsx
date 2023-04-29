c005db431edcec58a91269d441cd09fee0bd17d3
NemesisLW <86069181+NemesisLW@users.noreply.github.com>
2023-04-29 20:58:18 +0530

    whateve(2)

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -5,11 +5,43 @@ import { useLazyGetSummaryQuery } from "../services/article";
 
 const Sum = () => {
   const [article, setArticle] = useState({ url: "", summary: "" });
+  const [allArticle, setAllArticle] = useState([]);
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
+  useEffect(() => {
+    const articlesLocalStorage = JSON.parse(localStorage.getItem("articles"));
+    if (articlesLocalStorage) {
+      setAllArticle(articlesLocalStorage);
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const existingArticle = allArticle.find((item) => item.url === article.url);
+
+    if (existingArticle) return setArticle(existingArticle);
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticle];
+      setArticle(newArticle);
+      setAllArticle(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
   };
 
   return (
@@ -30,7 +62,7 @@ const Sum = () => {
             placeholder="Past the article link here..."
             value={article.url}
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
-            onKeyDown={hadleKeydown}
+            // onKeyDown={handleKeydown}
             required
             className="url_input peer"
           />
@@ -42,8 +74,54 @@ const Sum = () => {
           </button>
         </form>
         {/* URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticle.reverse().map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img
+                  src={copied === item.url ? tick : copy}
+                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* Display Result */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
